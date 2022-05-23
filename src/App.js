9b35c20249eac0eea48673fffdd02cac56137f3b
Osuohia Emmanuel <josephe442@gmail.com>
2022-05-23 14:23:29 +0100

    refactor the homePage and the design

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ExplorePage from './pages/ExplorePage'
 import HomePage from './pages/HomePage'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/explore" element={<ExplorePage />} />
-      </Routes>
+      <div className="flex flex-col justify-between h-screen">
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/explore" element={<ExplorePage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,23 @@
 import { Link } from 'react-router-dom'
+import profileImage from '../assets/images/twitterpic.jpg'
 
 function HomePage() {
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero h-full bg-base-200">
       <div className="hero-content text-center">
         <div className="max-w-md">
+          <div>
+            <img className='w-56 h-56 rounded-full mx-auto object-cover' src={profileImage} alt="profileImage" />
+          </div>
           <h1 className="text-5xl font-bold">Hello there</h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <Link to='/explore' className="btn btn-primary">Explore Event</Link>
+          <Link to="/explore" className="btn btn-primary">
+            Explore Event
+          </Link>
         </div>
       </div>
     </div>
